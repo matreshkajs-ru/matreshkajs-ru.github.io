f6827cba7d1903d3a6eb01f96cb2e1993e6a1efb
Andrey Gubanov <bookfast.org@gmail.com>
2015-02-17 12:25:04 +0200

    -

--- js/app.js
+++ js/app.js
@@ -7,3 +7,4 @@ require.config({
 });
 define( 'globals', {} )
 require(['app/main.class'], function( Main ) { window.app = new Main; });
+/*[].slice.call(document.querySelectorAll('[data-type]')).map( function(item) { return item.dataset.type}).filter(function(value, index, self) { return self.indexOf(value) === index; }).forEach(function(type) { var el = document.createElement('span'); el.dataset.type = el.innerHTML = type; document.querySelector('main').appendChild(el)})*/
---
+++ todo/js/app.js
@@ -0,0 +1,9 @@
+var ENTER_KEY = 13;
+var ESC_KEY = 27;
+
+(function (window) {
+	'use strict';
+
+	window.app = new Todos;
+
+})(window);
